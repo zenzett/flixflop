cd1bb84555a053eccf0c24da0daba56e9b9cbac5
andikadeniardi <andika.anggar@gmail.com>
2024-10-03 05:23:39 +0700

    feat: adding dropdown menu and tidy up text on  navbar.

--- app/Components/Navbar/index.tsx
+++ app/Components/Navbar/index.tsx
@@ -1,46 +1,194 @@
-import React from "react";
+// components/Navbar.js
+"use client";
+
+import Link from "next/link";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const Navbar = () => {
   return (
-    <div className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-20">
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
-              <h1 className="text-white text-lg font-bold">FilixFlop</h1>
+    <nav className="bg-gray-800 p-5">
+      <div className=" mx-auto flex items-center">
+        <div className="text-white text-2xl relative inline-block font-extrabold px-16 font-serif">
+          <Link href="/">FlixFlop</Link>
+        </div>
+        <div className="hidden md:flex items-center space-x-4 text-sm font-bold">
+          {/* Menu Button Movies */}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="text-white hover:bg-gray-700 px-3 py-2 rounded inline-flex items-center">
+                Movies
+              </MenuButton>
             </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Movies
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Tv Shows
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  People
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  More
-                </a>
-              </div>
+            <MenuItems className="absolute left-0 z-10 mt-2 w-48 origin-top-right bg-white border rounded shadow-lg">
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/popular"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Popular
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/nowplaying"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Now Playing
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/upcoming"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Up Coming
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/toprated"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Top Rated
+                  </Link>
+                )}
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+
+          {/* Menu Button Tv Shows */}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="text-white hover:bg-gray-700 px-3 py-2 rounded inline-flex items-center">
+                Tv Shows
+              </MenuButton>
             </div>
-          </div>
+            <MenuItems className="absolute left-0 z-10 mt-2 w-48 origin-top-right bg-white border rounded shadow-lg">
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/popular"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Popular
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/airingtoday"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Airing Today
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/ontv"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    On Tv
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/toprated"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Top Rated
+                  </Link>
+                )}
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+
+          {/* Menu Button People */}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="text-white hover:bg-gray-700 px-3 py-2 rounded inline-flex items-center">
+                People
+              </MenuButton>
+            </div>
+            <MenuItems className="absolute left-0 z-10 mt-2 w-48 origin-top-right bg-white border rounded shadow-lg">
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/popularpeople"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Popular People
+                  </Link>
+                )}
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+
+          {/* Menu Button More */}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <MenuButton className="text-white hover:bg-gray-700 px-3 py-2 rounded inline-flex items-center">
+                More
+              </MenuButton>
+            </div>
+            <MenuItems className="absolute left-0 z-10 mt-2 w-48 origin-top-right bg-white border rounded shadow-lg">
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/discussions"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Discussions
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/leaderboard"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Leaderboard
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/support"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Support
+                  </Link>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ active }) => (
+                  <Link
+                    href="/api"
+                    className={`block px-6 py-1 ${active && "bg-blue-100"}`}
+                  >
+                    Api
+                  </Link>
+                )}
+              </MenuItem>
+            </MenuItems>
+          </Menu>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
+
 export default Navbar;
