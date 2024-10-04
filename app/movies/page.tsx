4efffa5e069766f7f2390d91486b719a4439214d
Zain <zainazharul@gmail.com>
2024-10-04 22:45:49 +0700

    feat: add card in movies page

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,10 +1,12 @@
+import Card from "@/components/molecules/Card";
+
 export default function Movies() {
   return (
     <div
       className="flex flex-wrap md:flex-nowrap bg-dark-90"
       style={{ minHeight: "calc(100vh - 64px)" }}
     >
-      <span>Movies</span>
+      <Card title="Movies" />
     </div>
   );
 }
