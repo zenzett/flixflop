8f33a8db63e5ae96299eb0abd45ca3e9f2c491ac
andikadeniardi <andika.anggar@gmail.com>
2024-10-22 19:22:25 +0700

    feat: update Tv Shows page and remove unused files

---
+++ app/tvshows/page.tsx
@@ -0,0 +1,10 @@
+export default function TvShows() {
+  return (
+    <div
+      className="flex flex-wrap md:flex-nowrap bg-dark-90"
+      style={{ minHeight: "calc(100vh - 64px)" }}
+    >
+      <span>Tv Shows</span>
+    </div>
+  );
+}
